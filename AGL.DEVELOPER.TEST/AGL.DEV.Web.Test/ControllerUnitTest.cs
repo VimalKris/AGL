e8a80dba7acc9ef9e3af6f86d62fefbd73929e26
VimalKris <vimalkrishnank@gmail.com>
2017-01-08 05:53:53 +0530

    Initial commit

---
+++ AGL.DEVELOPER.TEST/AGL.DEV.Web.Test/ControllerUnitTest.cs
@@ -0,0 +1,149 @@
+﻿using AGL.DEV.Model;
+using AGL.DEV.Web.Controllers;
+using AGL.DEV.Web.Models;
+using AGL.DEV.Web.Services;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AGL.DEV.Web.Test
+{
+    [TestFixture]
+    public class ControllerUnitTest
+    {
+        private PetNameViewModel _petNameViewModel;
+        private PetNameViewModel _petCatNameViewModel;
+        private PetNameViewModel _petCatNameSortedViewModel;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            _petNameViewModel = new PetNameViewModel()
+            {
+                FemaleOwnerPetNames = new List<string>() { "Simba", "Nemo", "Jim" },
+                MaleOwnerPetNames = new List<string>() { "Garfield", "Fido", "Tom" }
+            };
+
+            _petCatNameViewModel = new PetNameViewModel()
+            {
+                FemaleOwnerPetNames = new List<string>() { "Simba", "Jim" },
+                MaleOwnerPetNames = new List<string>() { "Garfield", "Tom" }
+            };
+
+            _petCatNameSortedViewModel = new PetNameViewModel()
+            {
+                FemaleOwnerPetNames = new List<string>() { "Jim", "Simba" },
+                MaleOwnerPetNames = new List<string>() { "Garfield", "Tom" }
+            };
+        }
+
+        [Test]
+        public async Task Controller_Should_Return_ViewResult_With_Model()
+        {
+            // Arrange
+            Mock<IService> mockService = new Mock<IService>();
+            mockService.Setup(x => x.GetPetNameViewModel(PetType.Cat))
+                                    .Returns(async () =>
+                                    {
+                                        await Task.Yield();
+                                        return _petCatNameViewModel;
+                                    });
+            // Act
+            HomeController controller = new HomeController(mockService.Object);
+            ViewResult result = await controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result.ViewData.Model);             
+        }
+
+        [Test]
+        public async Task Controller_Should_Return_ViewResult_With_Valid_Model()
+        {
+            // Arrange
+            Mock<IService> mockService = new Mock<IService>();
+            mockService.Setup(x => x.GetPetNameViewModel(PetType.Cat))
+                                    .Returns(async () =>
+                                    {
+                                        await Task.Yield();
+                                        return _petCatNameViewModel;
+                                    });
+            // Act
+            HomeController controller = new HomeController(mockService.Object);
+            ViewResult result = await controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<PetNameViewModel>(result.ViewData.Model);
+        }
+
+        [Test]
+        public async Task Controller_Returned_ViewResult_With_Model_Having_Only_Cat_Names()
+        {
+            // Arrange
+            Mock<IService> mockService = new Mock<IService>();
+            mockService.Setup(x => x.GetPetNameViewModel(PetType.Cat))
+                                    .Returns(async () =>
+                                    {
+                                        await Task.Yield();
+                                        return _petCatNameViewModel;
+                                    });
+            PetNameViewModel expectedViewModel = _petCatNameViewModel;
+            
+            // Act
+            HomeController controller = new HomeController(mockService.Object);
+            ViewResult result = await controller.Index() as ViewResult;
+            PetNameViewModel actualViewModel = result.ViewData.Model as PetNameViewModel;
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedViewModel.FemaleOwnerPetNames, actualViewModel.FemaleOwnerPetNames);
+            CollectionAssert.AreEquivalent(expectedViewModel.MaleOwnerPetNames, actualViewModel.MaleOwnerPetNames);
+        }
+
+        [Test]
+        public async Task Controller_Returned_ViewResult_With_Model_Having_Only_Sorted_Cat_Names()
+        {
+            // Arrange
+            Mock<IService> mockService = new Mock<IService>();
+            mockService.Setup(x => x.GetPetNameViewModel(PetType.Cat))
+                                    .Returns(async () =>
+                                    {
+                                        await Task.Yield();
+                                        return _petCatNameViewModel;
+                                    });
+            PetNameViewModel expectedViewModel = _petCatNameSortedViewModel;
+            
+            // Act
+            HomeController controller = new HomeController(mockService.Object);
+            ViewResult result = await controller.Index() as ViewResult;
+            PetNameViewModel actualViewModel = result.ViewData.Model as PetNameViewModel;
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedViewModel.FemaleOwnerPetNames, actualViewModel.FemaleOwnerPetNames);
+            CollectionAssert.AreEquivalent(expectedViewModel.MaleOwnerPetNames, actualViewModel.MaleOwnerPetNames);
+        }
+
+        [Test]
+        public async Task Controller_Returned_ViewResult_With_Model_Having_Not_Only_Cat_Names()
+        {
+            // Arrange
+            Mock<IService> mockService = new Mock<IService>();
+            mockService.Setup(x => x.GetPetNameViewModel(PetType.Cat))
+                                    .Returns(async () =>
+                                    {
+                                        await Task.Yield();
+                                        return _petCatNameViewModel;
+                                    });
+            PetNameViewModel expectedViewModel = _petNameViewModel;
+            
+            // Act
+            HomeController controller = new HomeController(mockService.Object);
+            ViewResult result = await controller.Index() as ViewResult;
+            PetNameViewModel actualViewModel = result.ViewData.Model as PetNameViewModel;
+            
+            // Assert
+            CollectionAssert.AreNotEquivalent(expectedViewModel.FemaleOwnerPetNames, actualViewModel.FemaleOwnerPetNames);
+            CollectionAssert.AreNotEquivalent(expectedViewModel.MaleOwnerPetNames, actualViewModel.MaleOwnerPetNames);
+        }
+    }
+}
---
+++ AGL.DEVELOPER.TEST/AGL.DEV.Web.Test/ServiceUnitTest.cs
@@ -0,0 +1,137 @@
+﻿using AGL.DEV.Model;
+using AGL.DEV.Repository;
+using AGL.DEV.Web.Models;
+using AGL.DEV.Web.Services;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AGL.DEV.Web.Test
+{
+    [TestFixture]
+    public class ServiceUnitTest
+    {
+        private List<Person> _personData;
+        private List<string> _maleOwnerPetNames;
+        private List<string> _femaleOwnerPetNames;
+        private List<string> _maleOwnerPetNamesSorted;
+        private List<string> _femaleOwnerPetNamesSorted;
+        private List<string> _maleOwnerPetCatNames;
+        private List<string> _femaleOwnerPetCatNames;
+        private List<string> _maleOwnerPetCatNamesSorted;
+        private List<string> _femaleOwnerPetCatNamesSorted;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            _personData = new List<Person>()
+            {
+                new Person() { Name = "Alice", Age = 64, Gender = Gender.Female,
+                    Pets = new List<Pet>()
+                    {
+                        new Pet() { Name = "Simba", Type = PetType.Cat },
+                        new Pet() { Name = "Nemo", Type = PetType.Fish },
+                        new Pet() { Name = "Jim", Type = PetType.Cat }
+                    } },
+                new Person() { Name = "Bob", Age = 23, Gender = Gender.Male,
+                    Pets = new List<Pet>()
+                    {
+                        new Pet() { Name = "Garfield", Type = PetType.Cat },
+                        new Pet() { Name = "Fido", Type = PetType.Dog },
+                        new Pet() { Name = "Tom", Type = PetType.Cat },
+                    } },
+                new Person() { Name = "Steve", Age = 45, Gender = Gender.Male },
+            };
+
+            _maleOwnerPetNames = new List<string>() { "Garfield", "Fido", "Tom" };
+            _femaleOwnerPetNames = new List<string>() { "Simba", "Nemo", "Jim" };
+            _maleOwnerPetNamesSorted = new List<string>() { "Fido", "Garfield", "Tom" };
+            _femaleOwnerPetNamesSorted = new List< string > () { "Jim", "Nemo", "Simba"};
+            _maleOwnerPetCatNames = new List<string>() { "Garfield", "Tom" };
+            _femaleOwnerPetCatNames = new List<string>() { "Simba", "Jim" };
+            _maleOwnerPetCatNamesSorted = new List<string>() { "Garfield", "Tom" };
+            _femaleOwnerPetCatNamesSorted = new List<string>() { "Jim", "Simba" };
+        }
+
+        [Test]
+        public async Task Service_Should_Return_ViewModel()
+        {
+            // Arrange
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetPeopleData()).Returns(async () => { await Task.Yield(); return _personData; });
+
+            // Act
+            IService service = new Service(mockRepository.Object);
+            PetNameViewModel viewModel = await service.GetPetNameViewModel(PetType.Cat);
+
+            // Assert
+            // using of multiple asserts as they are testing the same thing (If the first assert fails, it means the second would fail too)
+            Assert.IsNotNull(viewModel);
+            Assert.IsNotNull(viewModel.FemaleOwnerPetNames);
+            Assert.IsNotNull(viewModel.MaleOwnerPetNames);
+        }
+
+        [Test]
+        public async Task Service_Returned_ViewModel_Has_Only_Cat_Names()
+        {
+            // Arrange
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetPeopleData()).Returns(async () => { await Task.Yield(); return _personData; });
+            PetNameViewModel expectedViewModel = new PetNameViewModel() {
+                FemaleOwnerPetNames = _femaleOwnerPetCatNames,
+                MaleOwnerPetNames = _maleOwnerPetCatNames
+            };
+            
+            // Act
+            IService service = new Service(mockRepository.Object);
+            PetNameViewModel actualViewModel = await service.GetPetNameViewModel(PetType.Cat);
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedViewModel.FemaleOwnerPetNames, actualViewModel.FemaleOwnerPetNames);
+            CollectionAssert.AreEquivalent(expectedViewModel.MaleOwnerPetNames, actualViewModel.MaleOwnerPetNames);
+        }
+
+        [Test]
+        public async Task Service_Returned_ViewModel_Has_Only_Sorted_Cat_Names()
+        {
+            // Arrange
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetPeopleData()).Returns(async () => { await Task.Yield(); return _personData; });
+            PetNameViewModel expectedViewModel = new PetNameViewModel()
+            {
+                FemaleOwnerPetNames = _femaleOwnerPetCatNamesSorted,
+                MaleOwnerPetNames = _maleOwnerPetCatNamesSorted
+            };
+
+            // Act
+            IService service = new Service(mockRepository.Object);
+            PetNameViewModel actualViewModel = await service.GetPetNameViewModel(PetType.Cat);
+            
+            // Assert
+            CollectionAssert.AreEqual(expectedViewModel.FemaleOwnerPetNames, actualViewModel.FemaleOwnerPetNames);
+            CollectionAssert.AreEqual(expectedViewModel.MaleOwnerPetNames, actualViewModel.MaleOwnerPetNames);
+        }
+
+        [Test]
+        public async Task Service_Returned_ViewModel_Not_Only_Has_Cat_Names()
+        {
+            // Arrange
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetPeopleData()).Returns(async () => { await Task.Yield(); return _personData; });
+            PetNameViewModel expectedViewModel = new PetNameViewModel()
+            {
+                FemaleOwnerPetNames = _femaleOwnerPetNames,
+                MaleOwnerPetNames = _maleOwnerPetNames
+            };
+            
+            // Act
+            IService service = new Service(mockRepository.Object);
+            PetNameViewModel actualViewModel = await service.GetPetNameViewModel(PetType.Cat);
+            
+            // Assert
+            CollectionAssert.AreNotEquivalent(expectedViewModel.FemaleOwnerPetNames, actualViewModel.FemaleOwnerPetNames);
+            CollectionAssert.AreNotEquivalent(expectedViewModel.MaleOwnerPetNames, actualViewModel.MaleOwnerPetNames);
+        }
+    }
+}
