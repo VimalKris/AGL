7fcac44641fd0b909269edf8e5f8041cd81fbb4f
VimalKris <vimalkrishnank@gmail.com>
2017-01-08 05:50:07 +0530

    Initial commit

---
+++ AGL.DEVELOPER.TEST/AGL.DEV.Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using AGL.DEV.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AGL.DEV.Repository
+{
+    public interface IRepository
+    {
+        Task<List<Person>> GetPeopleData();
+    }
+}
---
+++ AGL.DEVELOPER.TEST/AGL.DEV.Repository/WebServiceRepository.cs
@@ -0,0 +1,56 @@
+﻿using AGL.DEV.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AGL.DEV.Repository
+{
+    public class WebServiceRepository : IRepository
+    {
+        private string _webServiceUrl;
+        private HttpClient _httpClient;
+
+        public WebServiceRepository(string webServiceUrl, HttpClient httpClient)
+        {
+            if (webServiceUrl == null)
+                throw new ArgumentNullException("webServiceUrl");
+
+            if (httpClient == null)
+                throw new ArgumentNullException("httpClient");
+
+            _webServiceUrl = webServiceUrl;
+
+            if(_httpClient == null)
+                _httpClient = httpClient;
+        }
+
+        public async Task<List<Person>> GetPeopleData()
+        {
+            string jsonData = await GetPeopleDataAsync();
+
+            var jsonSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Converters = new[] { new StringEnumConverter() }
+            };
+
+            return JsonConvert.DeserializeObject<List<Person>>(jsonData, jsonSettings);
+        }
+
+        private async Task<string> GetPeopleDataAsync()
+        {
+            if (_webServiceUrl == null)
+                throw new Exception("Web Service Url not specified");
+
+            if (_httpClient == null)
+                throw new Exception("HttpClient instance not set");
+
+            HttpResponseMessage response = await _httpClient.GetAsync(_webServiceUrl);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();        
+        }
+    }
+}
