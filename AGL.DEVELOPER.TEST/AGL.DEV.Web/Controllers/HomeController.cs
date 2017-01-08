68d34e22b980f7bad2e98a338d85e9d4f6b5cfd5
VimalKris <vimalkrishnank@gmail.com>
2017-01-09 02:25:31 +0530

    Added exception handling

--- AGL.DEVELOPER.TEST/AGL.DEV.Web/Controllers/HomeController.cs
+++ AGL.DEVELOPER.TEST/AGL.DEV.Web/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using AGL.DEV.Web.Models;
 using AGL.DEV.Web.Services;
 using System;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace AGL.DEV.Web.Controllers
 {
+    [HandleError(View ="Error")]
     public class HomeController : Controller
     {
         private readonly IService _service;
@@ -22,8 +24,25 @@ namespace AGL.DEV.Web.Controllers
         [HttpGet]
         public async Task<ActionResult> Index()
         {
-            PetNameViewModel viewModel = await _service.GetPetNameViewModel(PetType.Cat);
-            return View(viewModel);
+             PetNameViewModel viewModel = await _service.GetPetNameViewModel(PetType.Cat);
+
+            return View("Index", viewModel);            
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.ExceptionHandled)
+                return;
+
+            Exception exception = filterContext.Exception;
+
+            filterContext.ExceptionHandled = true;
+
+            var Result = this.View("Error", new HandleErrorInfo(exception,
+                filterContext.RouteData.Values["controller"].ToString(),
+                filterContext.RouteData.Values["action"].ToString()));
+
+            filterContext.Result = Result;
         }
     }
 }
