bfbe1f0d5e91586bd9f7aeb9493e9c0011ca8df7
VimalKris <vimalkrishnank@gmail.com>
2017-01-08 05:50:51 +0530

    Initial commit

---
+++ AGL.DEVELOPER.TEST/AGL.DEV.Repository.Test/IntegrationTest.cs
@@ -0,0 +1,60 @@
+﻿using AGL.DEV.Model;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AGL.DEV.Repository.Test
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        private const string _webServiceUrl = "http://agl-developer-test.azurewebsites.net/people.json";
+        private static HttpClient _httpClient = new HttpClient();
+
+        [Test]
+        // Test if the http request has the right web service URL
+        public void WebService_Url_Should_Be_Valid()
+        {
+            // Act
+            var response = _httpClient.GetAsync(_webServiceUrl).Result;
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        // Test if the http request is using the right method to access the web service
+        public void WebService_HttpMethod_Should_Be_Get()
+        {
+            // Act
+            var response = _httpClient.GetAsync(_webServiceUrl).Result;
+            
+            // Assert
+            Assert.That(response.RequestMessage.Method, Is.EqualTo(HttpMethod.Get));
+        }
+
+        [Test]
+        // Test if the response content type is JSON (so that the response can be deserialized with a json deserializer)
+        public void WebService_ContentType_Should_Be_Json()
+        {
+            // Act
+            var response = _httpClient.GetAsync(_webServiceUrl).Result;
+            
+            // Assert
+            Assert.That(response.Content.Headers.ContentType.MediaType, Is.EqualTo("application/json"));
+        }
+
+        [Test]
+        public async Task Repository_Returns_Valid_Data()
+        {
+            // Act
+            IRepository repository = new WebServiceRepository(_webServiceUrl, _httpClient);
+            List<Person> personData = await repository.GetPeopleData();
+            
+            // Assert
+            Assert.That(personData.GetType(), Is.EqualTo(typeof(List<Person>)));
+        }
+    }
+}
---
+++ AGL.DEVELOPER.TEST/AGL.DEV.Repository.Test/UnitTest.cs
@@ -0,0 +1,59 @@
+﻿using AGL.DEV.Model;
+using Moq;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AGL.DEV.Repository.Test
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        private string _webServiceJson;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            _webServiceJson = @"[{""name"":""Alice"",""gender"":""Female"",""age"":64,""pets"":[{""name"":""Simba"",""type"":""Cat""},
+                                    {""name"":""Nemo"",""type"":""Fish""},{""name"":""Jim"",""type"":""Cat""}]}, 
+                                 {""name"":""Bob"",""gender"":""Male"",""age"":23,""pets"":[{""name"":""Garfield"",""type"":""Cat""},
+                                    {""name"":""Fido"",""type"":""Dog""},{""name"":""Tom"",""type"":""Cat""}]},
+                                 {""name"":""Steve"",""gender"":""Male"",""age"":45,""pets"":null}]";
+        }
+
+        [Test]
+        public async Task Repository_Returns_Valid_Data()
+        {
+            // Arrange
+            MockHttpMessageHandler mockHttpMessageHandler = new MockHttpMessageHandler();
+            mockHttpMessageHandler.When(HttpMethod.Get, "/test").Respond(HttpStatusCode.OK, "application/json", _webServiceJson);
+            Mock<HttpClient> mockHttpClient = new Mock<HttpClient>(mockHttpMessageHandler);
+            
+            // Act
+            IRepository repository = new WebServiceRepository("http://invalid/test", mockHttpClient.Object);
+            List<Person> personData = await repository.GetPeopleData();
+            
+            // Assert
+            Assert.That(personData.GetType(), Is.EqualTo(typeof(List<Person>)));
+        }
+
+        [Test]
+        public void Repository_Returns_Exception_When_WebService_Url_Is_Not_Found()
+        {
+            // Arrange
+            MockHttpMessageHandler mockHttpMessageHandler = new MockHttpMessageHandler();
+            mockHttpMessageHandler.When(HttpMethod.Get, "/test").Respond(HttpStatusCode.NotFound, "application/json", _webServiceJson);
+            Mock<HttpClient> mockHttpClient = new Mock<HttpClient>(mockHttpMessageHandler);
+            
+            // Act
+            IRepository repository = new WebServiceRepository("http://invalid/test", mockHttpClient.Object);
+            HttpRequestException httpRequestException = Assert.ThrowsAsync<HttpRequestException>(async () => await repository.GetPeopleData());
+            
+            // Assert
+            Assert.That(httpRequestException.Message, Is.EqualTo("Response status code does not indicate success: 404 (Not Found)."));
+        }
+    }
+}
